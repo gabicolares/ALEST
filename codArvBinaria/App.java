ca08d72c31881ada38b0d662f669559c3b18b247
gabicolares <105060450+gabicolares@users.noreply.github.com>
2022-10-18 09:30:07 -0300

    Aula do dia 18/10

---
+++ codArvBinaria/App.java
@@ -0,0 +1,36 @@
+import javax.sound.sampled.SourceDataLine;
+
+/**
+ * Classe App.
+ * @author Isabel H. Manssour
+ */
+
+public class App {
+
+    public static void main(String[] args) {
+        BinaryTreeOfInteger b = new BinaryTreeOfInteger();
+        b.addRoot(1);
+        b.addLeft(2, 1);
+        b.addRight(3, 1);
+        b.addRight(0, 3);
+        b.addLeft(4, 2);
+        b.addRight(5, 2);
+        b.addRight(6, 5);
+        b.addRight(7, 6);
+        
+        System.out.println("Total de nodos da arvore: " + b.size());
+
+        System.out.println("Caminhamentos pré-fixados");
+        System.out.println(b.positionsPre());
+
+        System.out.println("Caminhamentos pos-fixados");
+        System.out.println(b.positionsPos());
+
+        System.out.println("Caminhamentos central-fixados");
+        System.out.println(b.positionsCentral());
+
+
+        System.out.println("---------------------");
+        b.GeraDOT();
+    }
+}
